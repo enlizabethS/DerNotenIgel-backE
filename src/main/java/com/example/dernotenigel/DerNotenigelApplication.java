2832df2960e3ddcdc2a97fa78b874878044cc5ad
endorphinchik <elisaveta.stepura@mail.ru>
2024-10-18 05:22:38 +0300

    new architecture

--- src/main/java/com/example/dernotenigel/DerNotenigelApplication.java
+++ src/main/java/com/example/dernotenigel/DerNotenigelApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DernotenigelApplication {
+public class DerNotenigelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DernotenigelApplication.class, args);
+		SpringApplication.run(DerNotenigelApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/dernotenigel/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dernotenigel.repository;
-
-import com.example.dernotenigel.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
